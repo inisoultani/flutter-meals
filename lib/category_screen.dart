95ed0976e585a6856acd6044af2fb1a9e3eb521d
Muhammad Soultani <muhammad.soultani@tiket.com>
2021-09-13 16:07:19 +0700

    category homepage

---
+++ lib/category_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_meals/category_item.dart';
+import './dummy_data.dart';
+
+class CategoryScreen extends StatelessWidget {
+  const CategoryScreen({Key? key}) : super(key: key);
+
+  List<CategoryItem> renderCategoryItems() {
+    return DUMMY_CATEGORIES.map((category) =>
+        CategoryItem(title: category.title, color: category.color)).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('DailyMeal'),),
+      body: GridView(
+        padding: const EdgeInsets.all(20.0),
+        children: [...this.renderCategoryItems()],
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2, // for 200 widht, we had 300 height
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
